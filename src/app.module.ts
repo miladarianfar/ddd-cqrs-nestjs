c4d01311478edf1b1b3f28b8c26b70d9c8fff535
miladarianfar <miladarianfar8471@gmail.com>
2024-10-04 17:53:02 +0330

    feat: add all needed packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
 import { PersistenceModule } from './persistence/persistence.module';
 
 @Module({
   imports: [ApiModule, InfrastructureModule, PersistenceModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
